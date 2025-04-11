60f7e4ce00af1a5a8ca17e5789c4e91b39aab9b2
Gary Rutland <gary.rutland@dae.mn>
2025-04-11 15:49:05 +0100

    Added examples

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# terraform-aws-compute
+# AWS Compute Terraform module
+
+Terraform module which creates compute resources on AWS.
+
+## Usage
+
+Using the root wrapper module:
+
+```hcl
+module "compute" {
+  source  = "daemonite-labs/compute/aws"
+
+  ec2_bastion = {
+    identifier = "compute"
+    vpc_id     = "<VPC_ID>"
+    subnet_id  = "<SUBNET_ID>"
+  }
+}
+```
+
+Using the submodules directly:
+
+```hcl
+module "compute_ec2_bastion" {
+  source  = "daemonite-labs/compute/aws//modules/ec2-bastion"
+
+  identifier = "compute-ec2-bastion"
+  vpc_id     = "<VPC_ID>"
+  subnet_id  = "<SUBNET_ID>"
+}
+```
---
+++ examples/complete/main.tf
@@ -0,0 +1,45 @@
+locals {
+  region = "eu-west-1"
+
+  name = basename(path.cwd)
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Name       = local.name
+    Example    = "complete"
+    Repository = "github.com/daemonite-labs/terraform-aws-compute"
+  }
+}
+
+provider "aws" {
+  region = local.region
+
+  default_tags {
+    tags = local.tags
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs            = local.azs
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+}
+
+module "compute" {
+  source = "../../"
+
+  ec2_bastion = {
+    identifier = local.name
+    vpc_id     = module.vpc.vpc_id
+    subnet_id  = module.vpc.public_subnets[0]
+  }
+}
